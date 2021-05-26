1e2239543093d7e2f17794616498d848d577e965
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-26 16:32:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,11 +15,15 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  aoh = [ DON_G[:name], DON_G[:occupation], JOELLE_VD[:name],
-  JOELLE_VD[:occupation], PAT_M[:name], PAT_M[:occupation], 
-  KATE_G[:name], KATE_G[:occupation], BRUCE_G[:name], 
-  BRUCE_G[:occupation]
-  ]
+  aoh = [
+    { name:  "Don Gately", occupation: "Live-in Staff" }
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+    { name:  "Pat Monteseian", occupation: "Staff" }
+    { name:  "Kate Gompert", occupation: "None" }
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    
+    
+  
 end
 
 def aoh_lookup(aoh, row, key)
